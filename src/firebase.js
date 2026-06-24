b2e339799fd4ca9cad9d49f31c8a58d8c49b199b
psornsaree-debug <p.sornsaree@gmail.com>
2026-06-24 11:02:45 +0700

    Add files via upload

--- src/firebase.js
+++ src/firebase.js
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDT8M1eSnTR-6xUeP54g0irzif3o4kpcSI",
-  authDomain: "bookwyrm-ceaba.firebaseapp.com",
-  projectId: "bookwyrm-ceaba",
-  storageBucket: "bookwyrm-ceaba.firebasestorage.app",
-  messagingSenderId: "514700872109",
-  appId: "1:514700872109:web:a21f498e22d8914fd03924"
+  apiKey: "วาง_apiKey_ของคุณ",
+  authDomain: "ชื่อโปรเจกต์.firebaseapp.com",
+  projectId: "ชื่อโปรเจกต์",
+  storageBucket: "ชื่อโปรเจกต์.appspot.com",
+  messagingSenderId: "วาง_messagingSenderId",
+  appId: "วาง_appId",
 };
 
 const app = initializeApp(firebaseConfig);
