8158bd483dc850dff67d9d230ea1e138b77d6ced
psornsaree-debug <p.sornsaree@gmail.com>
2026-08-07 12:16:17 +0700

    Add files via upload

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,6 @@
-// ─────────────────────────────────────────────────────────────
-//  วางค่า config ของคุณจาก Firebase Console ตรงนี้
-//  (Project settings ⚙️ → General → Your apps → SDK setup and configuration → Config)
-// ─────────────────────────────────────────────────────────────
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
 const firebaseConfig = {
   apiKey: "AIzaSyDT8M1eSnTR-6xUeP54g0irzif3o4kpcSI",
   authDomain: "bookwyrm-ceaba.firebaseapp.com",
@@ -14,7 +9,7 @@ const firebaseConfig = {
   messagingSenderId: "514700872109",
   appId: "1:514700872109:web:a21f498e22d8914fd03924"
 };
-
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
 export const db = getFirestore(app);
